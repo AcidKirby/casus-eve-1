6b2c8e1c943d358b4a373a3a7c79339a2c1e2461
Arion Martha <s170016@student.windesheim.nl>
2024-10-07 00:08:36 +0200

    bewerk button werkt alleen bij attracties

--- Datapunten/Datapunt 3/Startcode beheerapplicatie/start_code.py
+++ Datapunten/Datapunt 3/Startcode beheerapplicatie/start_code.py
@@ -3,7 +3,7 @@ from tkinter_wrapper import Scherm
 
 # database aanmaken. 
 db = Database(host="localhost", gebruiker="user", wachtwoord="password", database="attractiepark_software")
-
+db.connect()
 
 # scherm aanmaken. Je mag de grootte van het scherm aanpassen.
 scherm = Scherm("Attractie beheer", 1250, 800)
@@ -14,14 +14,15 @@ attributen_attractie = [
     {"naam": "Id",  "type": "int", "read-only": True},
     {"naam": "Naam", "type": "string", "verplicht": True},
     {"naam": "Type", "type": "options", "opties": ["Achtbaan", "Water", "Draaien", "Familie", "Simulator"], "verplicht": True},
-    {"naam": "Minimale lengte", "type": "float"},
-    {"naam": "Maximale lengte", "type": "float"},
-    {"naam": "Minimale leeftijd", "type": "int"},
-    {"naam": "Maximale gewicht", "type": "int"},
     {"naam": "Overdekt", "type": "boolean", "verplicht": True},
     {"naam": "Gem. wachttijd", "type": "int","verplicht": False},
     {"naam": "Doorlooptijd", "type": "int", "verplicht": True},
     {"naam": "Actief", "type": "boolean","verplicht": True},
+    {"naam": "Minimale lengte", "type": "float"},
+    {"naam": "Maximale lengte", "type": "float"},
+    {"naam": "Minimale leeftijd", "type": "int"},
+    {"naam": "Maximale gewicht", "type": "int"},
+   
 ]
 
 attributen_horeca = [
@@ -39,57 +40,89 @@ attributen_horeca = [
 
 
 def attracties_ophalen():
-    db.connect()
+ Attractieresults = db.execute_query('SELECT id, naam, type, overdekt, geschatte_wachttijd, doorlooptijd, actief, attractie_min_lengte, attractie_max_lengte, attractie_min_leeftijd, attractie_max_gewicht FROM voorziening WHERE NOT type = "horeca" AND NOT type = "winkel" ;')
 
-
-    Attractieresults = db.execute_query('SELECT id, naam, type, overdekt, geschatte_wachttijd, doorlooptijd, actief, attractie_min_lengte, attractie_max_lengte, attractie_min_leeftijd, attractie_max_gewicht FROM voorziening WHERE NOT type = "horeca" AND NOT type = "winkel" ;')
     # vervang de voorbeeld data, door data uit de database.
-    return Attractieresults
+ return Attractieresults
+
 
 
 def horeca_ophalen():
 
-    Horecaresults = db.execute_query('SELECT id, naam, type, overdekt, geschatte_wachttijd, doorlooptijd, actief, productaanbod FROM voorziening WHERE type = "horeca" OR type = "winkel" ORDER BY type = "horeca"')
-    return Horecaresults
+ Horecaresults = db.execute_query('SELECT id, naam, type, overdekt, geschatte_wachttijd, doorlooptijd, actief, productaanbod FROM voorziening WHERE type = "horeca" OR type = "winkel" ORDER BY type = "horeca"')
 
-db.close()
-
+ return Horecaresults
 
 
-def voorziening_bewerken(bewerkte_voorziening, rij_index):
- """
-     UPDATE voorziening 
-     SET 
-     naam = %s, 
-     type = %s, 
-     overdekt = %s, 
-      geschatte_wachttijd = %s, 
-       doorlooptijd = %s, 
-      actief = %s, 
-      attractie_min_lengte = %s, 
-     attractie_max_lengte = %s, 
-      attractie_min_leeftijd = %s, 
-      attractie_max_gewicht = %s, 
-       productaanbod = %s 
-     WHERE 
-     id = %s
-      """
 
- params = (
-    bewerkte_voorziening[1],  # naam is a string
-    bewerkte_voorziening[2],  # type is a string
-    1 if bewerkte_voorziening[7].lower() == "ja" else 0,  # overdekt is stored as 1 or 0 (tinyint)
-    bewerkte_voorziening[8],  # wachttijd should be an integer
-    bewerkte_voorziening[9],  # doorlooptijd should be an integer
-    1 if bewerkte_voorziening[10].lower() == "ja" else 0,  # actief is stored as 1 or 0 (tinyint)
-    bewerkte_voorziening[3],
-    bewerkte_voorziening[4],
-    bewerkte_voorziening[5],
-    bewerkte_voorziening[6],
-    bewerkte_voorziening[11],
-    bewerkte_voorziening[0],
-            )
- print(f"Gewijzigde voorziening: {bewerkte_voorziening}")
+
+def voorziening_bewerken(bewerkte_voorziening,):
+     if "productaanbod" in bewerkte_voorziening:
+        bewerk_query_horeca = """
+        UPDATE voorziening 
+        SET 
+        naam = %s, 
+        type = %s, 
+        overdekt = %s, 
+        geschatte_wachttijd = %s, 
+        doorlooptijd = %s, 
+        actief = %s, 
+        productaanbod = %s
+        WHERE 
+        id = %s
+        """
+        params = (
+            bewerkte_voorziening["Naam"],  # naam is a string
+            bewerkte_voorziening["Type"],  # type is a string
+            1 if bewerkte_voorziening["Overdekt"] else 0,  # overdekt is stored as 1 or 0 (tinyint)
+            bewerkte_voorziening["Gem. wachttijd"],  # wachttijd should be an integer
+            bewerkte_voorziening["Doorlooptijd"],  # doorlooptijd should be an integer
+            1 if bewerkte_voorziening["Actief"] else 0,  # actief is stored as 1 or 0 (tinyint)
+            bewerkte_voorziening["Productaanbod"],
+            bewerkte_voorziening["Id"],
+        )
+ 
+        db.execute_query(bewerk_query_horeca, params)
+        scherm.herlaad_tabel(horeca_tabel, horeca_ophalen())
+    
+     else:
+        bewerk_query_attractie = """
+        UPDATE voorziening 
+        SET 
+        naam = %s, 
+        type = %s, 
+        overdekt = %s, 
+        geschatte_wachttijd = %s, 
+        doorlooptijd = %s, 
+        actief = %s, 
+        attractie_min_lengte = %s, 
+        attractie_max_lengte = %s, 
+        attractie_min_leeftijd = %s, 
+        attractie_max_gewicht = %s
+        WHERE 
+        id = %s
+        """
+
+        params = (
+            bewerkte_voorziening["Naam"],  # naam is a string
+            bewerkte_voorziening["Type"],  # type is a string
+            1 if bewerkte_voorziening["Overdekt"] else 0,  # overdekt is stored as 1 or 0 (tinyint)
+            bewerkte_voorziening["Gem. wachttijd"],  # wachttijd should be an integer
+            bewerkte_voorziening["Doorlooptijd"],  # doorlooptijd should be an integer
+            1 if bewerkte_voorziening["Actief"] else 0,  # actief is stored as 1 or 0 (tinyint)
+            bewerkte_voorziening["Minimale lengte"],
+            bewerkte_voorziening["Maximale lengte"],
+            bewerkte_voorziening["Maximale lengte"],
+            bewerkte_voorziening["Maximale gewicht"],
+            bewerkte_voorziening["Id"],
+        )
+ 
+        db.execute_query(bewerk_query_attractie, params)
+ 
+        scherm.herlaad_tabel(attractie_tabel,attracties_ophalen())
+        scherm.herlaad_tabel(horeca_tabel, horeca_ophalen())
+        print(f"Gewijzigde voorziening: {bewerkte_voorziening}")
+
     
    
 def voorziening_verwijderen(verwijderde_voorziening, rij_index):
@@ -107,7 +140,8 @@ def voorziening_verwijderen(verwijderde_voorziening, rij_index):
     else:
        scherm.toon_informatie_bericht("Let op!", "Voorziening niet verwijderd.")
        print("Niks is gewijzigd")
-       
+
+
 
       
     
